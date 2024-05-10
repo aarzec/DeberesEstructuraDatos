08a0b613186d375b8578392c19fb90bfeae005cc
Andrés <komodroid@outlook.com>
2024-05-10 00:11:13 -0500

    Subido ejercicio TDAs

---
+++ Unidad1/TDAs/Mat.h
@@ -0,0 +1,19 @@
+/***********************************************************************
+ * Universidad de las Fuerzas Armadas - ESPE
+ * Module:  Racional.h
+ * Author:  Andrés Romero
+ * Modified: jueves, 9 de mayo de 2024 04:16:58 p. m.
+ * Purpose: Declaration of the class Racional
+ ***********************************************************************/
+
+#if !defined(_Mat_H)
+#define _Mat_H
+
+class Mat
+{
+public:
+    static int minimoComunMultiplo(int n1, int n2);
+    static int maximoComunDivisor(int n1, int n2);
+};
+
+#endif
